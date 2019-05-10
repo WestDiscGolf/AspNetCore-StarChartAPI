131b64f235cd211ce08f06cb2158d8a0448de146
Adam Storr <astorr@gmail.com>
2019-05-10 23:39:44 +0100

    Completed the module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -18,5 +18,47 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var item = _context.CelestialObjects.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            item.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == item.Id).ToList();
+            return Ok(item);
+        }
+
+        [HttpGet("{name}", Name = "GetByName")]
+        public IActionResult GetByName(string name)
+        {
+            var items = _context.CelestialObjects.Where(x => x.Name == name).ToList();
+            if (!items.Any())
+            {
+                return NotFound();
+            }
+
+            foreach (var celestialObject in items)
+            {
+                celestialObject.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == celestialObject.Id).ToList();
+            }
+
+            return Ok(items);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var items = _context.CelestialObjects.ToList();
+            foreach (var celestialObject in items)
+            {
+                celestialObject.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == celestialObject.Id).ToList();
+            }
+
+            return Ok(items);
+        }
     }
 }
